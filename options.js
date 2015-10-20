e7ecf790352be925ea0e31028bd4e2b7ffbe3fc6
George Keith <ggkeithv@gmail.com>
2015-10-20 15:33:47 -0500

    add ability for protein goal to show in extension

--- options.js
+++ options.js
@@ -1,18 +1,18 @@
 $(function() {
   chrome.storage.sync.get("goal", function(items) {
-    $("#goal").text(items.goal);
+    $("#goal").val(items.goal);
   });
 
   $("#save").click(function(){
     var goal = $("#goal").val();
     if (goal) {
-      chrome.storage.sync.set({"goal": goal}, function() {
+      chrome.storage.sync.set({ "goal": goal }, function() {
         close();
       });
     }
   });
 
   $("#reset").click(function() {
-    chrome.storage.sync.set({"total": 0});
+    chrome.storage.sync.set({ "total": 0 });
   });
 });
